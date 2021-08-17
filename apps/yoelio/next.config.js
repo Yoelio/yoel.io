628bf8661c555ae5cfbe58bb82adbc57a645090b
yoelio <43119219+Yoelio@users.noreply.github.com>
2021-08-16 23:25:56 -0400

    add en-US locale to Next.js config

--- apps/yoelio/next.config.js
+++ apps/yoelio/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+    domains: [
+      {
+        domain: "yoel.io",
+        defaultLocale: "en-US",
+      },
+    ],
+  },
 };
